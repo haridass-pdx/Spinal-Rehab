bf0137f22764640f28d7aecbc7992a46ffa4dbd1
haridass-pdx <haridass@mac.com>
2026-06-04 21:02:56 -0700

    Initial Commit

---
+++ Spinal-Rehab/Spinal_RehabApp.swift
@@ -0,0 +1,17 @@
+//
+//  Spinal_RehabApp.swift
+//  Spinal-Rehab
+//
+//  Created by Hari Dass Khalsa on 6/4/26.
+//
+
+import SwiftUI
+
+@main
+struct Spinal_RehabApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Spinal-RehabTests/Spinal_RehabTests.swift
@@ -0,0 +1,19 @@
+//
+//  Spinal_RehabTests.swift
+//  Spinal-RehabTests
+//
+//  Created by Hari Dass Khalsa on 6/4/26.
+//
+
+import Testing
+@testable import Spinal_Rehab
+
+struct Spinal_RehabTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
